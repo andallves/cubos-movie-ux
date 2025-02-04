861361b80631e199f9309a33f89accf11102e257
andallves <andrealves.dev@gmail.com>
2025-02-04 08:39:29 -0300

    feat: criado footer

---
+++ src/templates/layout/Footer/index.tsx
@@ -0,0 +1,9 @@
+import {FooterContainer, Text} from "./styles.ts";
+
+export const Footer = () => {
+    return (
+        <FooterContainer>
+            <Text>2025 © Todos os direitos reservados a Cubos Movies</Text>
+        </FooterContainer>
+    )
+}
---
+++ src/templates/layout/Footer/styles.ts
@@ -0,0 +1,32 @@
+import styled, {css} from "styled-components";
+
+export const FooterContainer = styled.footer`
+    ${({ theme }) => css`
+        display: flex;
+        width: 100%;
+        min-width: 37.5rem;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: ${theme.spacings.xsmall};
+    
+        background: #121113;
+        border-top: 1px solid rgba(241, 230, 253, 0.19);
+        
+        @media (min-width: 768px) {
+            padding: ${theme.spacings.small};
+        }
+    `};
+`;
+
+export const Text = styled.span`
+    flex: 1 0 0;
+    align-self: stretch;
+    color: #B5B2BC;
+
+    text-justify: auto;
+    font-family: Montserrat, sans-serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+`
